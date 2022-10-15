206702c554d3d9fb326fe93a15603f581a23105a
Jordan <jordanshackelford2@gmail.com>
2022-10-15 13:56:44 +0700

    create CharactersInPlay.java, initialize private arraylists

---
+++ Java Programming: Arrays, Lists, and Structured Data/Week2/CharactersInPlay.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class CharactersInPlay{
+
+    private ArrayList<String> charNames;
+    private ArrayList<Integer> counts;
+
+    public CharactersInPlay(){
+        charNames = new ArrayList<String>();
+        counts = new ArrayList<Integer>();
+    }
+
+    public static void main(String[] args){
+
+    }
+}
