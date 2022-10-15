b1e3c748cbe80232e9782e038d380ada8c84d50d
Jordan <jordanshackelford2@gmail.com>
2022-10-15 13:58:24 +0700

    This method should update the two ArrayLists, adding the character’s name if it is not already there, and counting this line as one speaking part for this person.

--- Java Programming: Arrays, Lists, and Structured Data/Week2/CharactersInPlay.java
+++ Java Programming: Arrays, Lists, and Structured Data/Week2/CharactersInPlay.java
@@ -10,6 +10,18 @@ public class CharactersInPlay{
         counts = new ArrayList<Integer>();
     }
 
+    void update(String person){
+        int index = charNames.indexOf(person);
+        if(index == -1){
+            charNames.add(person);
+            counts.add(1);
+        }
+        else{
+            int value = counts.get(index);
+            counts.set(index, value+1);
+        }
+    }
+
     public static void main(String[] args){
 
     }
